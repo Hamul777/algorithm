be9e2d570a871da2639c49580e9fbb6446b81a41
Hamul777 <rhrb1162@gmail.com>
2024-04-06 20:27:57 +0900

    다시 시작
    cpp, java

---
+++ PROG/ch00/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int data1;
+int data2;
+
+int main() {
+ cin >> data1 >> data2;
+
+ cout << "data1 is " << 1;
+ cout << data1 << endl;
+    
+}  
---
+++ PROG/ch01_cout_cin/cout_cin.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int age = 17;
+    cout << sizeof(age) << endl;
+    cout << age << endl;
+    return 0;
+}
